a64ff77eb37bba8419a60b1155ed786d25ec02d3
guifurlan89 <gui.furlan.89@gmail.com>
2016-08-26 22:08:01 -0300

    Protótipos
    Adição de protótipo de postagem e circulo no mapa

--- app/lib/directives.js
+++ app/lib/directives.js
@@ -10,12 +10,12 @@ angular.module('starter.directives', [])
       function initialize() {
         navigator.geolocation.getCurrentPosition(function (pos) {
           var myLocation = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
-          
+
           var mapOptions = {
             center: myLocation,
             zoom: 18,
             mapTypeId: google.maps.MapTypeId.ROADMAP,
-            zoomControl: false,
+            zoomControl: true,
             mapTypeControl: false,
             scaleControl: false,
             streetViewControl: false,
@@ -23,6 +23,39 @@ angular.module('starter.directives', [])
           };
 
           var map = new google.maps.Map($element[0], mapOptions);
+
+          var populationOptions = {
+                strokeColor: "#FF0000",
+                strokeOpacity: 0.8,
+                strokeWeight: 2,
+                clickable: true,
+                fillColor: "#FFFFFF",
+                fillOpacity: 0.5,
+                map: map,
+                center: myLocation,
+                radius: 100
+            };
+            var infoWindow= new google.maps.InfoWindow({
+                content: "<b>Vagas:</b> Algumas<br/>" +
+                "<b>Flanelinhas:</b> Sim<br/>" +
+                "<b>Zona Azul:</b> Não<br/>" +
+                "<b>Risco de Inundação:</b> Sim<br/>" +
+                "<b>Região Perigosa:</b> Sim<br/>"
+                });
+
+            infoWindow.setPosition(myLocation);
+            infoWindow.open(map);
+
+            var cityCircle = new google.maps.Circle(populationOptions);
+
+            map.addListener('click', function(ev) {
+
+              cityCircle.setCenter(ev.latLng);
+              infoWindow.setPosition(ev.latLng);
+              infoWindow.open(map);
+
+            });
+
           $scope.onCreate({map: map});
 
           google.maps.event.addDomListener($element[0], 'mousedown', function (e) {
